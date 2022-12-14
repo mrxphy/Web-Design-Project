47ad750a36c3b05b2bb3c272e8fecc0e842f9473
mrxphy <115698196+mrxphy@users.noreply.github.com>
2022-12-14 23:54:13 +0000

    Update external.js

---
+++ external.js
@@ -0,0 +1,28 @@
+function validateForm() {
+  // Get the values of the input fields
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var age = document.getElementById('age').value;
+  var gender = document.querySelector('input[name="gender"]:checked').value;
+
+  // Validate the input fields
+  if (name == "") {
+    alert("Please enter your name");
+    return false;
+  }
+  if (email == "") {
+    alert("Please enter your email address");
+    return false;
+  }
+  if (age == "") {
+    alert("Please enter your age");
+    return false;
+  }
+  if (gender == undefined) {
+    alert("Please select your gender");
+    return false;
+  }
+
+  // If the form is valid, submit it
+  return true;
+}
